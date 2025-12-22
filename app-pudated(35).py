946ba16cad5ca59b2f6b78366c69eb6cedfc06f0
gurkkandurran-prog <gurkkandurran@gmail.com>
2025-12-22 11:36:54 +0300

    Add files via upload

--- app-pudated(35).py
+++ app-pudated(35).py
@@ -1,5 +1,3 @@
-[file name]: app-updated(35).py
-[file content begin]
 from valve_database import load_valves_from_excel, add_valve_to_database, delete_valve_from_database
 from valve import Valve
 from scipy.interpolate import CubicSpline
